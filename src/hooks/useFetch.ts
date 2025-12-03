08a5d71b215c6dff56655e9136277f9aa5381acd
Tanveer Anand <tanand1@hawk.iit.edu>
2025-12-03 14:23:03 -0800

    Custom-Hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,80 +2,69 @@ import { useEffect, useRef, useState } from "react";
 
 const cache = new Map<string, any>();
 
-interface FetchState<T> {data: T | null, isLoading: boolean, error: string | null}
+interface FetchState<T> { data: T | null, isLoading: boolean, error: string | null }
 
-const useFetch = <T> ({url}: {url:string}) : FetchState<T> => {
+const useFetch = <T>({ url }: { url: string }): FetchState<T> => {
 
-    const [fetchData, setFetchData] = useState<FetchState< T >>({
+    const [fetchData, setFetchData] = useState<FetchState<T>>({
         data: null,
         isLoading: true,
-        error:null
+        error: null
     })
-    const mountRef = useRef(true)
 
-
+    const mountRef = useRef(true) // component is mounted 
 
     useEffect(() => {
 
-        const controller = new AbortController(); 
-        const signal  = controller.signal
+        const controller = new AbortController();
+        const signal = controller.signal
 
         const cleanup = () => {
 
             controller.abort()
             mountRef.current = false
-    
-        }
-    
 
-        if(cache.has(url)){
-            setFetchData((prevState)=> { return {...prevState, data: cache.get(url), isLoading: false, error: null}} )
+        }
+
+        if (cache.has(url)) {
+            console.log(`[Cache] Found data for ${url}`);
+            setFetchData((prevState) => { return { ...prevState, data: cache.get(url), isLoading: false, error: null } })
             return cleanup;
         }
 
-        if(url) {
-            setFetchData((prevState)=> { return {...prevState, data: null, isLoading: true, error: null}} )
+        if (url) {
+            setFetchData((prevState) => { return { ...prevState, data: null, isLoading: true, error: null } })
         }
 
+        const fetchData = async () => {
 
+            try {
+                const response = await fetch(url, { signal: signal })
 
-        const fetchData = async () =>  {
+                if (!response.ok) {
+                    throw new Error("Request Failed " + response.statusText)
 
-            try{
-            const response = await fetch(url, {signal: signal})
+                }
 
-            if(!response.ok) {
-                throw new Error("Request Failed "+ response.statusText)
-              
+                const data: T = await response.json()
+
+                if (mountRef.current) {
+                    cache.set(url, data)
+                }
+
+                setFetchData((prevState) => { return { ...prevState, data: data, error: null, isLoading: false } })
             }
 
-            const data : T = await response.json()
+            catch (error: any) {
+                if (error.name === "AbortError") {
+                    console.log('Fetch aborted.');
+                }
+                else if (mountRef.current) {
 
-            if(mountRef.current) {
-                cache.set(url, data)
+                    setFetchData((prevState) => { return { ...prevState, data: null, error: error, isLoading: false } })
+                }
             }
-
-            setFetchData((prevState) => {return {...prevState,data:data,  error: null, isLoading: false}})
         }
-
-        catch(error:any) {
-            if(error.name === "AbortError") {
-            console.log('Fetch aborted.');
-            }
-            else if(mountRef.current) {
-            
-                setFetchData((prevState) => {return {...prevState,data:null,  error: error, isLoading: false}})
-            }
-        }
-    }
-
-
- 
-
-
-
-
-
 
         fetchData()
 
